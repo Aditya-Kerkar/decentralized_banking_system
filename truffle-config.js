67bd02f2a2ca5e001d8120739add027e7f15c5a6
Aditya-Kerkar <indraneelkerkar@gmail.com>
2025-04-01 19:40:31 +0530

    x

--- truffle-config.js
+++ truffle-config.js
@@ -3,7 +3,12 @@ module.exports = {
     development: {
       host: "127.0.0.1",
       port: 7545,
-      network_id: "*" // Match any network id
-    }
-  }
+      network_id: "*", // Match any network id
+    },
+  },
+  compilers: {
+    solc: {
+      version: "0.7.6", // Use a Solidity version that matches your contract (>=0.6.0 <0.8.0)
+    },
+  },
 };
